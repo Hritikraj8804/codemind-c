a35bdb3a87f81ce0b944b16bc58ca4b0bc46ad4c
Hritik Raj <37825309+Hritikraj8804@users.noreply.github.com>
2022-12-21 18:38:30 +0530

    #include<stdio.h> int main() {     int n,i,c=0;;     scanf("%d",&n);     int x[n];     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(i=0;i<n;i++)     {
            if(x[i]>=n)
            {
                c++;
            }
        }
        if(c==0) printf("YES");
        else printf("NO");
    }

---
+++ Can_Robber_Escape?.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0;;
+    scanf("%d",&n);
+    int x[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        
+        if(x[i]>=n)
+        {
+            c++;
+        }
+    }
+    if(c==0) printf("YES");
+    else printf("NO");
+}
