ea8038b040d2e56bbadf7ef17eb22603ac69d267
Hritik Raj <37825309+Hritikraj8804@users.noreply.github.com>
2023-05-01 21:54:01 +0530

    #include<stdio.h> int main() {     int n,i,j,max=0;     scanf("%d",&n);     int x[n];     for(i=0;i<n;i++)     scanf("%d",&x[i]);     for(i=0;i<n;i++)     {         int c=0;         for(j=i;j<n;j++)         {             if(x[j]==1) c++;             else break;         }         if(c>max) max=c;     }     printf("%d",max); }

--- Max_Consecutive_Ones.c
+++ Max_Consecutive_Ones.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {
-        int n,max=0,i,j;
-        scanf("%d",&n);
-        int x[n];
-        for(i=0;i<n;i++)
-            scanf("%d",&x[i]);
-            for(i=0;i<n;i++)
-            {
-                int c=0;
+    int n,i,j,max=0;
+    scanf("%d",&n);
+    int x[n];
+    for(i=0;i<n;i++)
+    scanf("%d",&x[i]);
+    for(i=0;i<n;i++)
+    {
+        int c=0;
         for(j=i;j<n;j++)
         {
-         if(x[j]==1) c++;
-         else break;
+            if(x[j]==1) c++;
+            else break;
         }
         if(c>max) max=c;
     }
