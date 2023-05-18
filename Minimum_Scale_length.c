85a4437bb64820d224cd4d6e160b2085ffd0e9cf
Hritik Raj <37825309+Hritikraj8804@users.noreply.github.com>
2023-05-18 17:29:01 +0530

    #include<stdio.h> int main() {     int n,i,j,max=9999,c;     scanf("%d",&n);     int x[n];     for(i=0;i<n;i++)     {     scanf("%d",&x[i]);     if(x[i]<max)     max=x[i];     }     for(j=max;j>=1;j--)     {         c=0;         for(i=0;i<n;i++)         {             if(x[i]%j==0)             {             c++;             }         }     if(c==n)     {         printf("%d",j);         break;     }     } }

--- Minimum_Scale_length.c
+++ Minimum_Scale_length.c
@@ -1,26 +1,29 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,k,n,min=9999,c;
+    int n,i,j,max=9999,c;
     scanf("%d",&n);
     int x[n];
     for(i=0;i<n;i++)
     {
-        scanf("%d",&x[i]);
-        if(x[i]<min) 
-        min=x[i];
+    scanf("%d",&x[i]);
+    if(x[i]<max)
+    max=x[i];
     }
-    for(j=min;j>=1;j--)
+    for(j=max;j>=1;j--)
     {
         c=0;
         for(i=0;i<n;i++)
         {
-            if(x[i]%j==0) c++;
+            if(x[i]%j==0)
+            {
+            c++;
+            }
         }
-        if(c==n)
-        {
-            printf("%d",j);
-            break;
-        }
+    if(c==n)
+    {
+        printf("%d",j);
+        break;
+    }
     }
 }
